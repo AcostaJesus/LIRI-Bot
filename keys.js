24941b626c5d9dd6ea703d8d32c4c7a15e613b50
Jesus Acosta <sparkiiyess@gmail.com>
2019-01-26 14:40:36 -0700

    Code uploaded

---
+++ keys.js
@@ -0,0 +1,23 @@
+// Spotify web API
+var SpotifyApi = require('node-spotify-api');
+
+var spotifyKeys = {
+  id: '6c014646f957407d94e28a7dd28c1b10',
+  secret: '26ebe83600fa40219759b5d1e5b6a76d'
+
+};
+
+
+// OMDB API
+var omdbapiKey = {
+	apiKey:"40e9cece"
+};
+
+// Export keys
+module.exports = {
+  // Spotify keys:
+  spotifyKeys: spotifyKeys,
+  // OMDB key:
+  omdbapiKey: omdbapiKey
+
+};
